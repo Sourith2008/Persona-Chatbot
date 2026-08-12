d82892d14833f2a05ebad3c45668ea6d9181ab82
Sourith2008 <sankardey1121990@gmail.com>
2026-08-12 15:44:55 +0530

    Updated Streamlit_ui.py

--- Streamlit_ui.py
+++ Streamlit_ui.py
@@ -1,21 +1,5 @@
 from app.inference import infer
 import streamlit as st
-def infer(user_input):
-    prompt=f'User: {user_input}\n Bot:'
-    inputs=tokenizer(prompt,return_tensors='pt')
-    outputs=model.generate(
-      **inputs,
-      do_sample=True,
-      top_k=5,
-      top_p=0.9,
-      temperature=0.7,
-      repetition_penalty=1.2,
-      max_length=50
-      )
-    input_length=inputs['input_ids'].shape[1]
-    response=tokenizer.decode(outputs[0][input_length:],skip_special_tokens=True)
-    response=response.strip()
-    return response
 st.title("🤖 Persona Chatbot")
 st.markdown("*A conversational AI fine-tuned on the Persona-Chat dataset*")
 with st.expander("ℹ️ About this project"):
